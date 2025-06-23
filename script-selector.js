96cd4866dd0c9470819a3896b12c3b332df32942
Judi06 <116767618+Judi06@users.noreply.github.com>
2025-06-23 14:50:27 +0200

    Update script-selector.js

--- script-selector.js
+++ script-selector.js
@@ -1,9 +1,11 @@
-        window.gtranslateSettings = {
-            default_language: "fr",
-            detect_browser_language: true,
-            languages: ["af","sq","am","ar","hy","az","eu","be","bn","bs","bg","ca","ceb","zh-CN","zh-TW","co","hr","cs","da","nl","en","eo","et","fi","fr","fy","gl","ka","de","el","gu","ht","ha","haw","he","hi","hmn","hu","is","ig","id","ga","it","ja","jv","kn","kk","km","rw","ko","ku","ky","lo","la","lv","lt","lb","mk","mg","ms","ml","mt","mi","mr","mn","my","ne","no","ny","or","ps","fa","pl","pt","pa","ro","ru","sm","gd","sr","st","sn","sd","si","sk","sl","so","es","su","sw","sv","tl","tg","ta","tt","te","th","tr","tk","uk","ur","ug","uz","vi","cy","xh","yi","yo","zu"],
-            wrapper_selector: "#floating-translate",
-            switcher_horizontal_position: "right",
-            switcher_vertical_position: "bottom",
-            floating_switcher_open_direction: "top"
-        };
+window.gtranslateSettings = {
+    default_language: "fr",
+    detect_browser_language: true,
+    languages: ["af","sq","am","ar","hy","az","eu","be","bn","bs","bg","ca","ceb","zh-CN","zh-TW","co","hr","cs","da","nl","en","eo","et","fi","fr","fy","gl","ka","de","el","gu","ht","ha","haw","he","hi","hmn","hu","is","ig","id","ga","it","ja","jv","kn","kk","km","rw","ko","ku","ky","lo","la","lv","lt","lb","mk","mg","ms","ml","mt","mi","mr","mn","my","ne","no","ny","or","ps","fa","pl","pt","pa","ro","ru","sm","gd","sr","st","sn","sd","si","sk","sl","so","es","su","sw","sv","tl","tg","ta","tt","te","th","tr","tk","uk","ur","ug","uz","vi","cy","xh","yi","yo","zu"],
+    wrapper_selector: "#floating-translate",
+    switcher_horizontal_position: "left",  /* Forcé à gauche */
+    switcher_vertical_position: "bottom",
+    floating_switcher_open_direction: "top",
+    switcher_close_on_scroll: true,  /* Fermer lors du défilement */
+    switcher_display_mode: "popup"   /* Mode popup pour économiser l'espace */
+};
